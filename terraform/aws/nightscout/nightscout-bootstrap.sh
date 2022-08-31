79650588407ae89eb0971f2303e919435c6acc8c
Petros Kolyvas <code@petros.io>
2022-08-30 22:11:24 -0300

    Forcing instance destroy properly

--- terraform/aws/nightscout/nightscout-bootstrap.sh
+++ terraform/aws/nightscout/nightscout-bootstrap.sh
@@ -6,9 +6,9 @@ echo NS-DOMAIN="${domain}" > /home/ubuntu/.profile
 echo NS-FEATURES="${features}" /home/ubuntu/.profile
 
 echo "Updating the system and installing Docker CE"
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install -y docker-ce docker-compose
+apt-get update
+apt-get install -y docker-ce docker-compose
 usermod -aG docker ubuntu
