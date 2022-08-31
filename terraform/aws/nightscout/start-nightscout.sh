2415edbbc743144c7a7d3aff090da304e7004f7f
Petros Kolyvas <code@petros.io>
2022-08-31 10:40:00 -0300

    Yet more debugging and destruction

--- terraform/aws/nightscout/start-nightscout.sh
+++ terraform/aws/nightscout/start-nightscout.sh
@@ -7,7 +7,12 @@ echo "Printing env vars"
 source /home/ubuntu/.profile
 env
 pwd
-
+echo "Ensuring docker is started"
+sudo systemctl start docker
+echo "Double checking groups"
+groups
+echo "Checking if docker is running"
+ps -aux | grep docker
 echo "Creating caddy network for Docker.."
 docker network create caddy
 echo "Running Docker Compose..."
