539cb8e342924cc417bc2d7f43c344789bd729bf
Petros Kolyvas <code@petros.io>
2022-08-30 21:35:02 -0300

    Forcing instance destroy

--- terraform/aws/nightscout/nightscout_instance.tf
+++ terraform/aws/nightscout/nightscout_instance.tf
@@ -1,50 +1,46 @@
-resource "aws_instance" "nightscout-central" {
-  ami                         = var.ami
-  associate_public_ip_address = true
-  instance_type               = var.instance_type
-  monitoring                  = false
-  key_name                    = var.launch_key
-  source_dest_check           = true
-  disable_api_termination     = true
-  tags = {
-    "Application" = "Nightscout"
-    "Name"        = "Nightscout and Mongo"
-    "Status"      = "Terraformed"
-  }
+# resource "aws_instance" "nightscout-central" {
+#   ami                         = var.ami
+#   associate_public_ip_address = true
+#   instance_type               = var.instance_type
+#   monitoring                  = false
+#   key_name                    = var.launch_key
+#   source_dest_check           = true
+#   disable_api_termination     = true
+#   tags = {
+#     "Application" = "Nightscout"
+#     "Name"        = "Nightscout and Mongo"
+#     "Status"      = "Terraformed"
+#   }
 
-  vpc_security_group_ids = var.security_groups
-  subnet_id              = var.subnet
-  lifecycle {
-    ignore_changes = [
-      ami,
-    ]
-  }
-  user_data = templatefile("nightscout/nightscout-bootstrap.sh", {
-    api_key  = var.api_key
-    domain   = var.domain
-    features = var.features
-  })
+#   vpc_security_group_ids = var.security_groups
+#   subnet_id              = var.subnet
+#   lifecycle {
+#     ignore_changes = [
+#       ami,
+#     ]
+#   }
+#   user_data = templatefile("nightscout/nightscout-bootstrap.sh", {
+#     api_key  = var.api_key
+#     domain   = var.domain
+#     features = var.features
+#   })
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = var.private_launch_key
-    host        = self.public_ip
-  }
-  provisioner "file" {
-    source      = "nightscout/docker-compose.yml"
-    destination = "/home/ubuntu/docker-compose.yml"
-  }
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"
+#     private_key = var.private_launch_key
+#     host        = self.public_ip
+#   }
+#   provisioner "file" {
+#     source      = "nightscout/docker-compose.yml"
+#     destination = "/home/ubuntu/docker-compose.yml"
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "docker network create caddy",
-      "docker-compose up -d"
-    ]
-  }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "docker network create caddy",
+#       "docker-compose up -d"
+#     ]
+#   }
 
-}
-
-# data "local_file" "nightscout-configure" {
-#   filename = "nightscout/nightscout-bootstrap.sh"
 # }
