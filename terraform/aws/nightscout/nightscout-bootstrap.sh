fc7ee275d1dbc3fa83391048eb3c8e9ec533cd89
Petros Kolyvas <code@petros.io>
2022-08-31 15:12:11 -0300

    Let's Nightscout...

--- terraform/aws/nightscout/nightscout-bootstrap.sh
+++ terraform/aws/nightscout/nightscout-bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Configuring the instance environment"
-echo 'export NS_API_KEY="${api_key}"' >> /home/ubuntu/.profile
-echo 'export NS_DOMAIN="${domain}"' >> /home/ubuntu/.profile
-echo 'export NS_FEATURES="${features}"' >> /home/ubuntu/.profile
-echo "export COMPOSE_HTTP_TIMEOUT=300" >> /home/ubuntu/.profile
+echo 'export NS_API_KEY="${api_key}"' >> /home/ubuntu/.env
+echo 'export NS_DOMAIN="${domain}"' >> /home/ubuntu/.env
+echo 'export NS_FEATURES="${features}"' >> /home/ubuntu/.env
+echo "export COMPOSE_HTTP_TIMEOUT=300" >> /home/ubuntu/.env
 
 echo "Updating the system and installing Docker CE"
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
