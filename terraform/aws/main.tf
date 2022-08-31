92ea45c01bab30bd96641e7dbd915002ca4f1fbd
Petros Kolyvas <code@petros.io>
2022-08-30 23:29:58 -0300

    more refactoring

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -1,18 +1,3 @@
-# Choose a region for your deployment
-# The Default Region is for the East Coast of the US
-#
-# Other options:
-# Asia Pacific = "ap-south-1"
-# Canada = "ca-central-1"
-# Europe = "eu-central-1"
-# US West = "us-west-2"
-#
-# Please note costs will differ depending on the regions you choose and these configurations
-# have not been tested outside of North American regions
-provider "aws" {
-  region = "us-east-2"
-}
-
 # Source for the VPC Module:
 # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/3.14.2
 module "vpc" {
