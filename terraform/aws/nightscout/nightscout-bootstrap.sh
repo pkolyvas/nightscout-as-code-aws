d25df0ad8abe7c03898c0a35dadb6be529d55b69
Petros Kolyvas <code@petros.io>
2022-08-31 14:00:05 -0300

    CREATE

--- terraform/aws/nightscout/nightscout-bootstrap.sh
+++ terraform/aws/nightscout/nightscout-bootstrap.sh
@@ -11,4 +11,3 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install -y docker-ce docker-compose
-sudo usermod -aG docker ubuntu
