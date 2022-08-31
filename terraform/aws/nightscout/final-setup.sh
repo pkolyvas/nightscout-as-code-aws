a82b1cafc3afc80c0f8e791253791516e23d508b
Petros Kolyvas <code@petros.io>
2022-08-31 14:38:00 -0300

    Giving us a little more time to get it right

--- terraform/aws/nightscout/final-setup.sh
+++ terraform/aws/nightscout/final-setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-echo "Sleeping to ensure completion of other initialization"
+
+# There's a lot of sleeping going on here. T3 instances, while fast once they've built up credits,
+# are not quick on boot. They need some time to "warm up". This means we get slightly unpredictable 
+# behaviour when trying to immediately start our docker stack.
+
+echo "Sleeping to ensure completion of other initialization processes..."
 sleep 35 #Ensuring the docker command is available
-echo "Adding ubuntu user to the docker group"
+echo "Adding ubuntu user to the docker group..."
 sudo usermod -aG docker ubuntu
-echo "Sleeping to ensure the next shell is ready"
+echo "Sleeping to ensure the next shell and additional software is ready..."
 sleep 35 #Ensuring the docker command is available
 echo "Done!"
-echo "Forcing reconnection"
+echo "Forcing reconnection...bye!"
 exit 
--- terraform/aws/nightscout/start-nightscout.sh
+++ terraform/aws/nightscout/start-nightscout.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# For debugging
-echo "Sleeping for 10 seconds..."
-sleep 10 #Ensuring the docker command is available
-# Debugging
-# echo "Printing env vars"
-# source /home/ubuntu/.profile
-# env
-# pwd
-echo "Ensuring docker is started"
-sudo systemctl start docker
+# There's a lot of sleeping going on here. T3 instances, while fast once they've built up credits,
+# are not quick on boot. They need some time to "warm up". This means we get slightly unpredictable 
+# behaviour when trying to immediately start our docker stack.
+
+echo "Sleeping for 60 seconds to make sure everything settles down following first-boot..."
+sleep 60 #Ensuring the docker command is available
 echo "Double checking groups"
 groups
 echo "Checking if docker is running"
 ps -aux | grep docker
 echo "Creating caddy network for Docker.."
+sleep 5
 docker network create caddy
+sleep 5
 echo "Running Docker Compose..."
+sleep 5
 docker-compose -f /home/ubuntu/docker-compose.yml up -d
 echo "Done."
