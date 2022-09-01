cc3164b88cf3e113e6b8338069999facea7299f7
Petros Kolyvas <code@petros.io>
2022-09-01 13:38:42 -0300

    Trying out a bash function to solve to sleep-time brittleness

--- terraform/aws/nightscout/final-setup.sh
+++ terraform/aws/nightscout/final-setup.sh
@@ -2,14 +2,28 @@
 
 # There's a lot of sleeping going on here. T3 instances, while fast once they've built up credits,
 # are not quick on boot. They need some time to "warm up". This means we get slightly unpredictable 
-# behaviour when trying to immediately start our docker stack.
+# behaviour when trying to immediately start our docker stack. This is even worse when we run T3 instances
+# in "standard" credit mode vs. the higher performance "unlimited" mode. See the Terraform root module variable 
+# declarations to control the credit specification.
 
-echo "Sleeping to ensure completion of other initialization processes..."
-sleep 60 #Ensuring the docker command is available
+# Beacause Terraform doesn't wait for CloudInit (via user_data) to complete before starting the provisioner process, 
+# we need to find a way to wait an indeterminate amount of time. Using `sleep` exclusively is brittle. 
+# At the same time this janky function risks getting stuck in a loop forever if something goes wrong.
+checkDocker () {
+    if pgrep -x "dockerd" >/dev/null
+    then
+        echo "Docker is running..."
+    else
+        echo "Oops. Docker is not running yet. Sleep time..."
+        sleep 15
+        checkDocker        
+    fi
+}
+
+echo "Checking for the installation of Docker to complete..."
+checkDocker
 echo "Adding ubuntu user to the docker group..."
 sudo usermod -aG docker ubuntu
-echo "Sleeping to ensure the next shell and additional software is ready..."
-sleep 30 #Ensuring the docker command is available
 echo "Done!"
 echo "Forcing reconnection...bye!"
 exit 
--- terraform/aws/nightscout/start-nightscout.sh
+++ terraform/aws/nightscout/start-nightscout.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# There's a lot of sleeping going on here. T3 instances, while fast once they've built up credits,
-# are not quick on boot. They need some time to "warm up". This means we get slightly unpredictable 
-# behaviour when trying to immediately start our docker stack.
-
-echo "Sleeping for 60 seconds to make sure everything settles down following first-boot..."
-sleep 30 #Ensuring the docker command is available
-echo "Double checking groups"
-groups
-echo "Checking if docker is running"
-ps -aux | grep docker
 echo "Creating caddy network for Docker.."
-sleep 5
 docker network create caddy
-sleep 5
 echo "Running Docker Compose..."
 docker-compose -f /home/ubuntu/docker-compose.yml up -d
 echo "Done!"
