075d0bd980eedcfabffa9f29a688bbacc39fc6a2
Petros Kolyvas <code@petros.io>
2022-09-01 21:18:14 -0300

    Small grammar fix

--- terraform/aws/nightscout/start-nightscout.sh
+++ terraform/aws/nightscout/start-nightscout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Creating caddy network for Docker.."
+echo "Creating caddy network for Docker..."
 docker network create caddy
 echo "Running Docker Compose..."
 docker-compose -f /home/ubuntu/docker-compose.yml up -d
