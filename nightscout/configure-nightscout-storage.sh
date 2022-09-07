412e03a5649adb6e1976702d8295d4bc70401bd0
Petros Kolyvas <code@petros.io>
2022-09-07 08:39:43 -0300

    #4 - storage director as bash variable

--- nightscout/configure-nightscout-storage.sh
+++ nightscout/configure-nightscout-storage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DATADIR = /nightscout_data
+
 checkMongoMount () {
     if findmnt --target /data > /dev/null; then
     echo "Mongo data directory mounted..."
@@ -12,11 +14,11 @@ checkMongoMount () {
         sudo mkfs.ext4 /dev/nvme1n1p1
         echo "Adding disk to the filesystem table..."
         OUTPUT="$(sudo blkid -s UUID -o value /dev/nvme1n1p1)"
-        echo "UUID=$OUTPUT    /data   ext4    defaults    0    1" | sudo tee -a /etc/fstab
+        echo "UUID=$OUTPUT    $DATADIR  ext4    defaults    0    1" | sudo tee -a /etc/fstab
         echo "Configuring mount directory..."
-        sudo mkdir -p /nightscout_data/
+        sudo mkdir -p $DATADIR
         echo "Mounting Mongo data directory..."
-        sudo mount /nightscout_data
+        sudo mount $DATADIR
         echo "Checking mount..."
         #checkMongoMount
     fi
