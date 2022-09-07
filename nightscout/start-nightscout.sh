07163db1a65ad03aa14ea87245f25a7e835be99d
Petros Kolyvas <code@petros.io>
2022-09-07 11:05:37 -0300

    Fixing the AWS query for the running instance

--- nightscout/start-nightscout.sh
+++ nightscout/start-nightscout.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-INSTANCE_ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=Hightscout --region $AWS_DEFAULT_REGION | grep -o -E '\"i-[a-z0-9]*\"' | grep -o 'i-[a-z0-9]*'`
+INSTANCE_ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=Nightscout Name=instance-state-name,Values=running --region ${AWS_DEFAULT_REGION} | grep -o -E '\"i-[a-z0-9]*\"' | grep -o 'i-[a-z0-9]*'`
 
 echo "Remove credit limitation for update process..."
-aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=unlimited --no-cli-pager --region $AWS_DEFAULT_REGION
+aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=unlimited --no-cli-pager --region ${AWS_DEFAULT_REGION}
 sleep 5 # Buffer to esure the throttle is removed.
 
 echo "Creating caddy network for Docker..."
@@ -12,6 +12,6 @@ echo "Running Docker Compose..."
 docker-compose -f /home/ubuntu/docker-compose.yml up -d
 
 echo "Changing EC2 instance credit spec to standard..."
-aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=standard --no-cli-pager --region $AWS_DEFAULT_REGION
+aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=standard --no-cli-pager --region ${AWS_DEFAULT_REGION}
 
 echo "Done!"
