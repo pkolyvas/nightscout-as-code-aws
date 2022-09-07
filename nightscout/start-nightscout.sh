f62441db10f69e4ba60ed586ed534b70e9f8d04e
Petros Kolyvas <code@petros.io>
2022-09-07 14:52:32 -0300

    We likely don't need to add unlimited credit spec

--- nightscout/start-nightscout.sh
+++ nightscout/start-nightscout.sh
@@ -4,10 +4,6 @@ source ~/.env
 
 INSTANCE_ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=Nightscout Name=instance-state-name,Values=running --region ${AWS_DEFAULT_REGION} | grep -o -E '\"i-[a-z0-9]*\"' | grep -o 'i-[a-z0-9]*'`
 
-echo "Remove credit limitation for update process..."
-aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=unlimited --region ${AWS_DEFAULT_REGION}
-sleep 5 # Buffer to esure the throttle is removed.
-
 echo "Creating caddy network for Docker..."
 docker network create caddy
 echo "Running Docker Compose..."
