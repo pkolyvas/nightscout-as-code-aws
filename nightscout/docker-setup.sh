36200e004414e4e2d03ea5b593afdb390a87c36c
Petros Kolyvas <code@petros.io>
2022-09-07 08:30:21 -0300

    #9 - Trying a new approach with APT

--- nightscout/docker-setup.sh
+++ nightscout/docker-setup.sh
@@ -9,19 +9,6 @@
 # Beacause Terraform doesn't wait for CloudInit (via user_data) to complete before starting the provisioner process, 
 # we need to find a way to wait an indeterminate amount of time. Using `sleep` exclusively is brittle. 
 # At the same time this janky function risks getting stuck in a loop forever if something goes wrong.
-checkDocker () {
-    if pgrep -x "dockerd" >/dev/null
-    then
-        echo "Docker is running..."
-    else
-        echo "Oops. Docker is not running yet. Waiting..."
-        sleep 15
-        checkDocker        
-    fi
-}
-
-echo "Checking for the installation of Docker to complete..."
-checkDocker
 echo "Adding ubuntu user to the docker group..."
 sudo usermod -aG docker ubuntu
 echo "Done!"
