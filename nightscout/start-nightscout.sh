fe5796fb6a7de95ec465936178a150102c37acc7
Petros Kolyvas <code@petros.io>
2022-09-07 10:37:45 -0300

    #7 - solved performance inconsistency with credits

--- nightscout/start-nightscout.sh
+++ nightscout/start-nightscout.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-INSTANCE_ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=Hightscout | grep -o -E '\"i-[a-z0-9]*\"' | grep -o 'i-[a-z0-9]*'`
+INSTANCE_ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=Hightscout --region | grep -o -E '\"i-[a-z0-9]*\"' | grep -o 'i-[a-z0-9]*'`
 
+echo "Remove credit limitation for update process..."
 aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=unlimited --no-cli-pager --region $AWS_DEFAULT_REGION
+sleep 5 # Buffer to esure the throttle is removed.
 
 echo "Creating caddy network for Docker..."
 docker network create caddy
