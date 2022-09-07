b3f4237d6a859eb198c9fff0d4b19618d554233c
Petros Kolyvas <code@petros.io>
2022-09-06 21:38:08 -0300

    storage fix

--- nightscout/configure-nightscout-storage.sh
+++ nightscout/configure-nightscout-storage.sh
@@ -9,13 +9,14 @@ checkMongoMount () {
         sudo parted -s -a optimal /dev/nvme1n1 mklabel gpt
         sudo parted -s -a optimal /dev/nvme1n1 mkpart primary ext4 0% 100%
         echo "Formatting disk..."
+        sudo mkfs.ext4 /dev/nvme1n1p1
         echo "Adding disk to the filesystem table..."
-        OUTPUT="$(sudo blkid -s UUID -o value /dev/sdc$partnumber)"
+        OUTPUT="$(sudo blkid -s UUID -o value /dev/nvme1n1p1)"
         echo "UUID=$OUTPUT    /data   ext4    defaults    0    1" | sudo tee -a /etc/fstab
         echo "Mounting Mongo data directory"
         sudo mount /data
         echo "Checking mount..."
-        checkMongoMount
+        #checkMongoMount
     fi
 }
 
