8601ab2a90e9f169027cd988e4b7c135b088ec00
Petros Kolyvas <code@petros.io>
2022-09-07 10:29:45 -0300

    Fixing credits issues

--- nightscout/start-nightscout.sh
+++ nightscout/start-nightscout.sh
@@ -2,17 +2,14 @@
 
 INSTANCE_ID=`aws ec2 describe-instances --filters Name=tag:Name,Values=Hightscout | grep -o -E '\"i-[a-z0-9]*\"' | grep -o 'i-[a-z0-9]*'`
 
+aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=unlimited --no-cli-pager --region $AWS_DEFAULT_REGION
+
 echo "Creating caddy network for Docker..."
 docker network create caddy
 echo "Running Docker Compose..."
 docker-compose -f /home/ubuntu/docker-compose.yml up -d
 
 echo "Changing EC2 instance credit spec to standard..."
-aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=standard --no-cli-pager
-
-echo "Remove AWS credentials from intance environment & files..."
-sed -i '5,6d' ~/.env
-unset AWS_ACCESS_KEY_ID
-unset AWS_SECRET_ACCESS_KEY
+aws ec2 modify-instance-credit-specification --instance-credit-specifications InstanceId=$INSTANCE_ID,CpuCredits=standard --no-cli-pager --region $AWS_DEFAULT_REGION
 
 echo "Done!"
