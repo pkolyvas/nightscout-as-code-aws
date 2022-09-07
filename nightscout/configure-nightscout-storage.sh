1801814b87d376ee8c43ab34f4fbe1476dcc14a6
Petros Kolyvas <code@petros.io>
2022-09-07 09:12:12 -0300

    #4 - parameterized

--- nightscout/configure-nightscout-storage.sh
+++ nightscout/configure-nightscout-storage.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 DATADIR = /nightscout_data
+EBS_DEV = /dev/nvme1n1
 
 checkMongoMount () {
     if findmnt --target /data > /dev/null; then
-    echo "Mongo data directory mounted..."
+        echo "Mongo data directory mounted..."
+    # elif  blkid -o value -s TYPE $EBS_DEV 
+    #     echo "Checking for unmounted mongo volume..."
     else
         echo "Mongo data is not mounted. Configuring..."
         echo "Partitioning disk..."
-        sudo parted -s -a optimal /dev/nvme1n1 mklabel gpt
-        sudo parted -s -a optimal /dev/nvme1n1 mkpart primary ext4 0% 100%
+        sudo parted -s -a optimal $EBS_DEV mklabel gpt
+        sudo parted -s -a optimal $EBS_DEV mkpart primary ext4 0% 100%
         echo "Formatting disk..."
         sudo mkfs.ext4 /dev/nvme1n1p1
         echo "Adding disk to the filesystem table..."
-        OUTPUT="$(sudo blkid -s UUID -o value /dev/nvme1n1p1)"
+        OUTPUT="$(sudo blkid -s UUID -o value ${EBS_DEV}p1)"
         echo "UUID=$OUTPUT    $DATADIR  ext4    defaults    0    1" | sudo tee -a /etc/fstab
         echo "Configuring mount directory..."
         sudo mkdir -p $DATADIR
