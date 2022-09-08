1527de93251ad0eafeddc8f304947132dfd9c821
Petros Kolyvas <code@petros.io>
2022-09-08 09:26:47 -0300

    #12 - Good old spelling mistake

--- nightscout/nightscout-userdata.sh
+++ nightscout/nightscout-userdata.sh
@@ -19,7 +19,7 @@ echo 'export AWS_DEFAULT_REGION="${region}"' >> /home/ubuntu/.env
 echo 'export MONGO_IMAGE="${mongo_image}"' >> /home/ubuntu/.env
 echo 'export MONGO_IMAGE_TAG="${mongo_image_tag}"' >> /home/ubuntu/.env
 echo 'export NIGHTSCOUT_IMAGE="${nightscout_image}"' >> /home/ubuntu/.env
-echo 'export NIGHTSCOUT_IMAGE_TAG="${nighscout_image_tag}"' >> /home/ubuntu/.env
+echo 'export NIGHTSCOUT_IMAGE_TAG="${nightscout_image_tag}"' >> /home/ubuntu/.env
 
 cat /home/ubuntu/.env >> /home/ubuntu/.profile
 
